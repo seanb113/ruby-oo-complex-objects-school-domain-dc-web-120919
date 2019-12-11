29e65d943f9c5187617f71ce5372691282f2db19
Sean Beach <sean.beach1@yahoo.com>
2019-12-11 22:43:20 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
-# code here!
+class School
+  attr_accessor :roster, :grade, :name
+  attr_reader :school
+
+  def initialize(school, student, grade, roster)
+    @name = name
+    @roster = roster
+    @grade = grade
+    @name = name
+  end
+
+  def roster
+    roster = {}
+  end
+  
+  def sort
+    sorted = {}
+    roster.each do |'grade', 'name'|
+  end
+end
